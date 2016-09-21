e327dd690b2e6de8071046099cb10053922b38a0
b8bauer <bbauer@cca.edu>
2016-09-21 01:06:26 -0700

    Update inclass_1.js

--- inclass_1.js
+++ inclass_1.js
@@ -1,21 +1,20 @@
-function setup (){
-	createCanvas(500,500);
-	background(0);
-}
-function draw (){	
-	
-	var numberOfOrbs = 0;
-	var orbX = 20;
-	var orbY = 20;
-	
-	while (numberOfOrbs <= 22){
-	
-		fill(255);
-		stroke(255);
-		ellipse(orbX,orbY,20);
-		
-		numberOfOrbs = numberOfOrbs + 1;
-		orbX = numberOfOrbs*20;
-		orbY = orbX;
-	}
+function setup(){
+	createCanvas(200, 200);
+	background(255);
+
+	var i = 0;
+
+	line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
+	i = i + 1;
+		line(10+i*30, 10, 10+i*30, height-10);
 }
