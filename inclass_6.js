b780be1e6e06938ea60221846359b1c99e6b3364
b8bauer <bbauer@cca.edu>
2016-09-21 01:07:31 -0700

    Create inclass_6.js

---
+++ inclass_6.js
@@ -0,0 +1,13 @@
+function setup(){
+	createCanvas(1000, 1000);
+	background(167);
+
+	for (var lineRepeat = 0; lineRepeat <= 20; lineRepeat++){
+		for (var xCoordinate = 50; xCoordinate <= 650; xCoordinate = xCoordinate + 10){
+			line (xCoordinate, 50, 350, 350)
+		}
+		for (var xCoordinate = 50; xCoordinate <= 650; xCoordinate = xCoordinate + 10){
+			line (xCoordinate, 650, 350, 350)
+		}
+	}
+}
