581afd9320564ca249aa543947049566099573a4
b8bauer <bbauer@cca.edu>
2016-09-21 01:06:05 -0700

    Update inclass_2.js

--- inclass_2.js
+++ inclass_2.js
@@ -1,28 +1,8 @@
-function setup() {
-  createCanvas (500,500);
-  background(0);
-}
+function setup(){
+	createCanvas(200, 200);
+	background(255);
 
-function draw() {
-  var rows = 0;
-  var columns = 0;
-  var x = 25;
-  var y = 25;
-  
-  while (columns < 23){
-  	
-    while (rows < 23){
-	  	fill (255);
-	  	stroke (255);
-	  	ellipse (x,y,15);
-	  	rows = rows + 1;
-	  	y = y + 20;
-	  }
-  
-  rows = 0;
-  columns = columns + 1;
-  x = x + 20;
-  y = 25;
-  
-  }
+	for (var i = 10; i <= 180; i = i + 10){
+		line (i, i, i+10, i);
+	}
 }
